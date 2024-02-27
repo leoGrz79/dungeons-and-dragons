7285d6a04b82b321e70209b580bb65798c7c3cfc
Leonardo Grzybowski <leonardogrz@hotmail.com>
2024-02-27 15:44:01 -0300

    Req. 01 DONE

---
+++ src/Races/Race.ts
@@ -0,0 +1,23 @@
+export default abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(raceName: string, raceDexterity: number) {
+    this._name = raceName;
+    this._dexterity = raceDexterity;
+  }
+
+  get name():string {
+    return this._name;
+  }
+
+  get dexterity():number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
